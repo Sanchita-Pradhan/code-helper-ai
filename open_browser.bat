b8bafb7ad8b5d03aca3fac6b118340e184228fcc
Sanchita-Pradhan <pradhansanchita353@gmail.com>
2025-05-28 14:31:44 +0530

    Fix browser opening issues and add separate browser opener

---
+++ open_browser.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Opening browser to http://127.0.0.1:5000
+start "" http://127.0.0.1:5000
+echo Browser should be opening now.
+pause
--- run_app.bat
+++ run_app.bat
@@ -4,6 +4,12 @@ echo.
 echo Please wait while the application starts...
 echo.
 cd /d "%~dp0"
-start "" http://127.0.0.1:5000
+echo Starting Flask server...
+echo.
+echo IMPORTANT: Once the server is running, open your web browser and go to:
+echo http://127.0.0.1:5000
+echo.
+echo You can also run open_browser.bat to open the browser automatically.
+echo.
 python app.py
 pause
